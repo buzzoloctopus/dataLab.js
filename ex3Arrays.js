544154964a3e54a848a72e5dbc41aa7f78798ff9
buzzoloctopus <bojan@wishbonecouriers.com>
2016-09-30 15:11:28 -0500

    extracting strings
    .substr(x,y) will start at x and return y number of characters
    .substring(x,y) will start at x and stop at y

---
+++ ex3Arrays.js
@@ -0,0 +1,6 @@
+var text = 'Mr.Roo won\'t stop chasing the other animals around the house!'
+
+console.log(text.substr(2,5));
+console.log(text.slice(2,5));
+console.log(text.substring(2,5));
+console.log(text.length);
